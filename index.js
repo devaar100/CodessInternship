0319583e3e53d3a3a1d8bd31ecddd31c23099331
devaar100 <developer.aarnav100@gmail.com>
2020-10-31 12:08:20 +0530

    style fix

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ const CHECKBOX = "CHECKBOX"
 
 const data = [
     {
+        question: "What is your choice of beverage ?",
         type: LIST,
         options: ["Tea", "Coffee", "Soft drinks", "Water"],
         responses: ["Coffee", "Coffee", "Tea", "Soft drinks", "Coffee", "Coffee", "Water"]
     },
     {
+        question: "What is your favourite travel destination ?",
         type: MCQ,
         options: ["England", "Australia", "UAE", "Malaysia"],
         responses: ["England" , "Norway", "England", "France", "England", "Malaysia", "UAE"]
     },
     {
+        question: "Which chocolates do you like ?",
         type: CHECKBOX,
         options: ["Dairy milk", "Five star", "Milky bar", "Munch", "KitKat"],
         responses: [["Five star", "Milky bar", "Munch", "KitKat"],
@@ -40,53 +43,7 @@ function listProcessing(listData) {
         map.set(responses[i], map.get(responses[i]) + 1)
     }
 
-    const data = [[],[]]
-    map.forEach((value, key) => {
-        data[0].push(key)
-        data[1].push(value)
-    })
-
-    const graph = document.createElement("canvas")
-    const ctx = graph.getContext('2d');
-    const chart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: data[0],
-            datasets: [{
-                label: 'Graph for beverage choices',
-                data: data[1],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        stepSize: 1,
-                        max: Math.max(...data[1])+1,
-                        min: 0
-                    }
-                }]
-            }
-        }
-    });
-    document.body.append(graph)
+    appendGraph(map,listData.question, 'bar',LIST)
 }
 
 function checkboxProcessing(checkboxData) {
@@ -103,7 +60,7 @@ function checkboxProcessing(checkboxData) {
         }
     }
 
-    console.log(map)
+    appendGraph(map,checkboxData.question, 'horizontalBar',CHECKBOX)
 }
 
 function mcqProcessing(mcqData) {
@@ -124,7 +81,71 @@ function mcqProcessing(mcqData) {
         }
     }
 
-    console.log(map)
+    appendGraph(map,mcqData.question,'doughnut',MCQ)
+}
+
+function appendGraph(map, question, chartType, type) {
+
+    const data = [[],[]]
+    map.forEach((value, key) => {
+        data[0].push(key)
+        data[1].push(value)
+    })
+
+    const div = document.createElement("div")
+    div.className = "ChartContainer"
+    document.body.append(div)
+    const title = document.createElement("h4")
+    title.innerText = question
+    div.append(title)
+    const graph = document.createElement("canvas")
+    const ctx = graph.getContext('2d');
+    const chart = new Chart(ctx, {
+        type: chartType,
+        data: {
+            labels: data[0],
+            datasets: [{
+                label: '',
+                data: data[1],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        stepSize: 1,
+                        max: Math.max(...data[1])+1,
+                        min: 0
+                    }
+                }],
+                xAxes: [{
+                    ticks: {
+                        stepSize: 1,
+                        max: Math.max(...data[1])+1,
+                        min: 0
+                    }
+                }]
+            }
+        }
+    });
+    div.append(graph)
 }
 
 window.onload = () => {
@@ -142,10 +163,3 @@ window.onload = () => {
         }
     })
 }
-
-// listProcessing(item)
-// BarChart
-// mcqProcessing(item)
-// BarChart
-// checkboxProcessing(item)
-// Doughnut / Piechart
