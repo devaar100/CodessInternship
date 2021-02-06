99550c2e3534bf8e95c6c6aeb5229fe18861188b
devaar100 <developer.aarnav100@gmail.com>
2021-02-06 13:53:55 +0530

    excel file read

--- index.js
+++ index.js
@@ -148,7 +148,47 @@ function appendGraph(map, question, chartType, type) {
     div.append(graph)
 }
 
+var ExcelToJSON = function() {
+
+    this.parseExcel = function(file) {
+        var reader = new FileReader();
+        const result = []
+        reader.onload = function(e) {
+            var data = e.target.result;
+            var workbook = XLSX.read(data, {
+                type: 'binary'
+            });
+            workbook.SheetNames.forEach(function(sheetName) {
+                // Here is your object
+                var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+                // var json_object = JSON.stringify(XL_row_object);
+                // var json_string = JSON.parse(json_object);
+                result.push(XL_row_object)
+            })
+        };
+
+        reader.onerror = function(ex) {
+            console.log(ex);
+        };
+
+        reader.readAsBinaryString(file);
+        return result
+    };
+};
+
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+    var xl2json = new ExcelToJSON();
+    const res = xl2json.parseExcel(files[0]);
+    console.log(res)
+    // Add logic here to render charts from excel data
+    // You can modify excel file yourself
+}
+
 window.onload = () => {
+    document.getElementById('upload').addEventListener('change', handleFileSelect, false);
+
+    // You can remove this after your data is done
     data.forEach(item => {
         switch (item.type) {
             case LIST:
